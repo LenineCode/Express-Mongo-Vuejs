797cab71fc3f16532be3bf1d594cff33cc84e173
apinot <antoine.pinot1@gmail.com>
2020-03-03 20:35:35 +0100

    create api

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        name: 'api',
+        version: '1.0.0',
+    });
+})
+
+
+app.listen(8080);
